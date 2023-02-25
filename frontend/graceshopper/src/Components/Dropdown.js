654fc2c13dd3501fe75a2f250777d680aad9a627
chunkay1 <fabian.hernandez1713@gmail.com>
2023-02-25 10:06:09 -0600

    compoform nent rendering inside of drop down menu about to start custom theme

---
+++ frontend/graceshopper/src/Components/Dropdown.js
@@ -0,0 +1,14 @@
+function Dropdown() {
+    return (
+        <div>
+            <form>
+                <label>Test</label>
+                <input></input>
+                <button>Push Me</button>
+            </form>
+
+        </div>
+    )
+}
+
+export default Dropdown;
--- frontend/graceshopper/src/Components/Test.js
+++ frontend/graceshopper/src/Components/Test.js
@@ -1,24 +1,38 @@
-
+import Dropdown from "./Dropdown"
 
 function Test() {
     return (
         <div>
-            <div class="container text-center text-bg-primary">
-                <div class="row">
-                    <div class="col">
-                        Column
-                    </div>
-                    <div class="col">
-                        Column
-                    </div>
-                    <div class="col">
-                        Column
+            <nav class="navbar navbar-expand-lg bg-body-tertiary text-bg-primary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Navbar</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Features</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Pricing</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown link
+                                </a>
+                                <ul class="dropdown-menu">
+                                    {/* <li><a class="dropdown-item" href="#">Action</a></li> */}
+                                    <li><Dropdown /></li>
+                                </ul>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-            </div>
-            <h1>
-                Poop!
-            </h1>
+            </nav>
 
 
         </div>
