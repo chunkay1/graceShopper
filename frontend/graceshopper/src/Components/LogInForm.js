92139335d985736b81cde9912e879369281a387a
chunkay1 <fabian.hernandez1713@gmail.com>
2023-02-28 10:01:26 -0600

    mostly finished navbar - minor bug with background color on the log in drop down

--- frontend/graceshopper/src/Components/LogInForm.js
+++ frontend/graceshopper/src/Components/LogInForm.js
@@ -1,9 +1,19 @@
 import styles from './LogInForm.module.css'
+import { useState } from 'react';
+
+const setTargetValue = (callback) => {
+    return (event) => {
+        callback(event.target.value)
+    }
+}
 
 function LogInForm() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+
     return (
         <div className={styles.container}>
-            <h4>Log In</h4>
             <form
                 onSubmit={
                     (event) => {
@@ -12,29 +22,39 @@ function LogInForm() {
                     }
                 }
             >
-                <div className={styles.input}>
+                <div className={styles.inputDivs}>
                     <input
                         type="text"
                         name='username'
                         placeholder='Enter Username'
                         id='username'
+                        value={username}
+                        onChange={setTargetValue(setUsername)}
                         required
                     />
                 </div>
 
-                <div className={styles.input}>
+                <div className={styles.inputDivs}>
                     <input
                         type="password"
                         name='password'
                         placeholder='Enter Password'
                         id='password'
+                        value={password}
+                        onChange={setTargetValue(setPassword)}
                         required
                     />
                 </div>
 
+
                 <div className={styles.submit}>
                     <button className={styles.button}>Log In</button>
                 </div>
+
+                <p className={styles.secondHeader}>Don't Have an Account?
+
+                    <a className={styles.signUpLink} href='www.google.com'>Create One Here!</a>
+                </p>
             </form>
 
         </div>
--- frontend/graceshopper/src/Components/NavBar.js
+++ frontend/graceshopper/src/Components/NavBar.js
@@ -1,43 +1,90 @@
 import LogInForm from "./LogInForm"
 import styles from './NavBar.module.css'
+import "bootstrap-icons/font/bootstrap-icons.css"
 
 function NavBar() {
     return (
         <div>
             <nav class="navbar navbar-expand-lg bg-success">
                 <div class="container-fluid">
-                    <a class="navbar-brand text-white" href="#">Hike & Seek</a>
-                    <a class="navbar-brand" href="#">
-                        <img className={styles.icon} src="SolidForest.png" alt="Forest" width="45" height="36"></img></a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-white" href="#">Features</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-white" href="#">Pricing</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown link
-                                </a>
-                                <ul class="dropdown-menu">
-                                    {/* <li><a class="dropdown-item" href="#">Action</a></li> */}
-                                    <li><LogInForm /></li>
-                                </ul>
-                            </li>
-                        </ul>
+
+                    <div className={styles.TitleandIcon}>
+                        <a
+                            class="navbar-brand text-white"
+                            href="home">Hike & Seek</a>
+                        <a
+                            class="navbar-brand"
+                            href="#">
+                            <img
+                                className={styles.icon}
+                                src="SolidForest.png"
+                                alt="Forest"
+                                width="45"
+                                height="36">
+                            </img>
+                        </a>
+                    </div>
+
+
+                    <div className={styles.links}>
+                        <button
+                            class="navbar-toggler"
+                            type="button"
+                            data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+                            aria-label="Toggle navigation">
+
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div
+                            class="collapse navbar-collapse" id="navbarNavDropdown">
+
+                            <ul class="navbar-nav">
+
+                                <li class="nav-item">
+                                    <a
+                                        class="nav-link active text-white" aria-current="page"
+                                        href="home">Home
+                                    </a>
+                                </li>
+
+                                {/* <li className={`nav-item dropdown ${styles.dropdown}`}> */}
+                                <li className={`nav-item dropdown`}>
+                                    <a
+                                        class="nav-link dropdown-toggle text-white"
+                                        href="#"
+                                        role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Log In
+                                    </a>
+
+                                    <ul
+                                        className={`dropdown-menu dropdown-menu-lg-end ${styles.dropdown}`}>
+                                        {/* <li><a class="dropdown-item" href="#">Action</a></li> */}
+                                        <li><LogInForm /></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a
+                                        class="nav-link text-white" href="products">Products
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a
+                                        class="nav-link text-white"
+                                        href="cart">
+
+                                        <i className={`bi bi-cart text-white ${styles.cartIcon}`}></i>
+
+                                    </a>
+                                </li>
+
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </nav>
 
-            <div class="p-3 mb-2 bg-primary text-white">.bg-primary</div>
+
 
         </div>
     )
