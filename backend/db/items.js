2b3e2bad488e5a5a62d6d33e67fb8fd0348adba5
Samson343 <sam.ban@gmail.com>
2023-03-03 15:12:47 -0700

    beefed up the seed data!
    plenty of items, categories, and most importantly, AI generated images that look great

--- backend/db/items.js
+++ backend/db/items.js
@@ -2,14 +2,14 @@ const client = require('./client')
 
 //unsure how to handle uuid, should this happen in javascript or sql- in this file, in this function, or elsewhere?
 
-async function adminCreateItem ({ name, category, brand, size, price }) {
+async function adminCreateItem ({ name, category, brand, size, price, image }) {
     try {
         const { rows: [item] } = await client.query(`
-            INSERT INTO items (name, category, brand, size, price)
-            VALUES($1, $2, $3, $4, $5)
+            INSERT INTO items (name, category, brand, size, price, image)
+            VALUES($1, $2, $3, $4, $5, $6)
             ON CONFLICT (name) DO NOTHING
             RETURNING *;
-        `, [name, category, brand, size, price])
+        `, [name, category, brand, size, price, image])
 
         return item
 
