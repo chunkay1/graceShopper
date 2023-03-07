a9d0e7bc251378776f4bc91d31aba382a7e73284
chunkay1 <fabian.hernandez1713@gmail.com>
2023-03-07 09:42:47 -0600

    changes to useEffect and useState in frontend/Products.js to allow for filtering by category

--- frontend/graceshopper/src/Components/Products.js
+++ frontend/graceshopper/src/Components/Products.js
@@ -7,19 +7,30 @@ import { setTargetValue } from '../constants/constants';
 
 
 const Products = ({token}) => {
-    const [products, setProducts] = useState([]);
     const [category, setCategory] = useState('');
     const [cartItemProps, setCartItemProps] = useState({});
+    
+    const [products, setProducts] = useState([]);
+    
+    useEffect(() => {
+        const getProductsAsync = async () => {
+            let allProducts = await getAllItems();
+            let categoryItems = allProducts.filter(product => product.category === category)
+            console.log(categoryItems)
+            setProducts(categoryItems);
+        }
+        getProductsAsync();
+    }, [category])
 
     useEffect(() => {
         const getProductsAsync = async () => {
             let products = await getAllItems();
             setProducts(products);
-            console.log('products are', products)
+            // console.log('products are', products)
         }
         getProductsAsync();
     }, [])
-
+    
     let getCategoryItems = async (category)=> {
         await getProductsByCategory(category);
     } 
@@ -45,62 +56,33 @@ const Products = ({token}) => {
         <div>
 
             <h1>Products</h1>
-
-            {/* <button 
-                onClick={ (e) => {
-                    getProductsAsync();
-                }}>
-                Click me twice
-            </button> */}
-            
-            <br/>
-
-            {/* <button 
-                onClick={ (e) => {
-                    products.map(({brand, category, id, name, price, size}) => {
-                        return (
-                            <div>
-                                <h3>{brand}</h3>
-                                <h3>{category}</h3>
-                                <h3>{id}</h3>
-                                <h3>{name}</h3>
-                                <h3>{price}</h3>
-                                <h3>{size}</h3>
-                            </div>
-                        )
-                    
-                    })
-                }}>
-                Click me Once?
-            </button> */}
-            
-            <button onClick={(e) => { console.log('all')}}>View All Products</button>
-            
-            <button 
-                
-                onClick={(e) => { 
-                setTargetValue(setCategory)
-                getCategoryItems('shoes');
-                }}
-            >
-                    
-                Shoes
-                    
-            </button>
             
             <button onClick={(e) => { 
-                setTargetValue(setCategory)
+                window.location.reload();
+            }}>View All Products</button>
+            
+            <button onClick={(e) => { 
+                getCategoryItems('shoes');
+                setCategory('shoes');
+
+            }}>Shoes</button>
+            
+            <button onClick={(e) => { 
+                // setTargetValue(setCategory)
                 getCategoryItems('tents');
+                setCategory('tents');
                 }}>tents</button>
             
             <button onClick={(e) => { 
                 setTargetValue(setCategory)
                 getCategoryItems('Clothing');
+                setCategory('Clothing');
             }}>Clothing</button>
             
             <button onClick={(e) => { 
-                setTargetValue(setCategory)
+                // setTargetValue(setCategory)
                 getCategoryItems('Firepits and Grills');
+                setCategory('Firepits and Grills');
             }}>Grills and firepits</button>
 
 
@@ -110,15 +92,15 @@ const Products = ({token}) => {
                 {
                     products.map(({brand, category, id, name, price, size, image}) => {
                         return (
-                            <div>
+                            <div key={id}>
 
                                 <div 
-                                    class={`card ${styles.productCard}`}
-                                    
+                                    class={`card ${styles.productCard}`} 
                                     style={{
                                         width: "18rem",
                                         backgroundColor: "#B7E4C7"
                                     }}>
+
                                     <img src={image} class="card-img-top" alt="..."/>
                                     
                                     <div class="card-body">
@@ -139,7 +121,9 @@ const Products = ({token}) => {
                                                 onClick={async (event) => {
                                                     event.preventDefault();
                                                     console.log('added to cart')
-                                                    await addToCart(getCartItemProps(brand, category, id, name, price, size, image)                                                    )
+                                                    let test = await addToCart(getCartItemProps(brand, category, id, name, price, size, image))
+                                                    console.log(test)
+                                                                                                      
                                                     // getCartItemProps(brand, category, id, name, price, size, image).then((result) => {
                                                     //     console.log(result)
                                                     //     addToCart(result);
@@ -152,42 +136,8 @@ const Products = ({token}) => {
                                         </div>
                                     </div>
 
-
                                 </div>
 
-                            {/* <div 
-                                className={styles.productCard} 
-                                key={id}>
-                                    <img src = {image} alt = {'shoes png'} width = {"100%"} className = {styles.image}></img>
-                                    <div className={styles.description}>
-                                      <h3 className={styles.header}>Brand: <p className={styles.cardValue}>{brand}</p></h3>
-                                      <h3 className={styles.header}>Category: <p className={styles.cardValue}>{category}</p></h3>
-                                      <h3 className={styles.header}>Id: <p className={styles.cardValue}>{id}</p></h3>
-                                      <h3 className={styles.header}>Name: <p className={styles.cardValue}>{name}</p></h3>
-                                      <h3 className={styles.header}>Price: <p className={styles.cardValue}>{price}</p></h3>
-                                      <h3 className={styles.header}>Size: <p className={styles.cardValue}>{size}</p></h3>
-                                    </div>
-
-                                    {
-                                        (!token)
-
-                                        ?
-
-                                        null
-                                        :
-
-                                        <div className={styles.cartbutton}> 
-                                            <button
-                                                onClick={(event) => {
-                                                    event.preventDefault();
-                                                    console.log('added to cart')
-                                                }}>
-                                                Add to Cart!
-                                            </button>
-                                        </div>
-
-                                    }
-                            </div> */}
                             </div>
 
                         )
--- frontend/graceshopper/src/api/cartRequests.js
+++ frontend/graceshopper/src/api/cartRequests.js
@@ -1,13 +1,14 @@
 export async function addToCart({brand, category, id, name, price, size, image}) {
     try {
-        console.log(`cart item is 
-            Brand: ${brand}, 
+        return (
+            `Brand: ${brand}, 
             category : ${category},
             id : ${id},
             name : ${name},
             price : ${price},
             size : ${size}`  
-        )
+        ) 
+        
     } catch (error) {
         console.error(error)
     }
