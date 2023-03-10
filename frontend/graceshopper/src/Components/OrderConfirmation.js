1c08288662ba3e615ae5b540227a32a347e741ab
Kendall Hudson <64994077+kendallhudson@users.noreply.github.com>
2023-03-09 20:27:00 -0600

    order confirmation page styling

--- frontend/graceshopper/src/Components/OrderConfirmation.js
+++ frontend/graceshopper/src/Components/OrderConfirmation.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from '../styles/OrderConfirmation.module.css'
+import { Link } from 'react-router-dom';
 
 const OrderConfirmation = () => {
 
@@ -7,12 +8,22 @@ const OrderConfirmation = () => {
      
     return (
         // bootstrap styling 
+        <div>
         <div id='container' class={`order-confirmation-container ${styles.ordercontainer}`}>
-            <span>
-            <h1>Order Confirmation</h1>
-            <h1>Thank you for your order!</h1>
-    <p>Your order confirmation number is: {confirmationNumber}</p>
-    </span>
+            
+            <h1 class={`h1-order-received ${styles.h1OrderReceived}`}>Your order has been received</h1>
+            <img class={`img-checkmark ${styles.imgCheckmark}`} src='https://cdn-icons-png.flaticon.com/512/4436/4436481.png' alt='Green checkmark'/>
+            <h1 class={`h1-thank-you ${styles.h1ThankYou}`}>Thank you for your purchase!</h1>
+            <p class={`p-confirmation-num ${styles.pConfirmationNum}`}>Your order confirmation number is: {confirmationNumber}</p>
+            
+            <Link to={'/products'}>
+              <button class="w-100 btn btn-warning btn-lg" type="submit">
+                  Continue Shopping</button>
+            </Link>
+
+    
+        </div>
+        
         </div>
         
     )
