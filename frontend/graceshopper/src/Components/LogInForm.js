6702982df0d7c2f3d7dea49516316becdb0d905e
Samson343 <sam.ban@gmail.com>
2023-03-15 19:19:15 -0600

    2 quick fixes
    behaves properly if login is unsuccessful and assigns isAdmin to false by default

--- frontend/graceshopper/src/Components/LogInForm.js
+++ frontend/graceshopper/src/Components/LogInForm.js
@@ -25,7 +25,9 @@ function LogInForm({ loginDropdown, setLoginDropdown }) {
                         console.log('Submit Form')
                         await logIn({username,password}).then(async (response) => {
                             alert(response.message)
-                            setLoginDropdown(false)
+                            if (response.token) {
+                                setLoginDropdown(false)
+                            }
 
                             await myProfile(response.token).then ((data) => {
                                 if (data === true) {
