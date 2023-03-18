2839b096616abdb2904d49b1ffd62574b523cadd
Vivienne Chawande <113947648+vivienne9@users.noreply.github.com>
2023-03-18 13:46:37 -0500

    styling, fixed jsx errors, added get products button to add the delete button on the products in Admin page

--- frontend/graceshopper/src/Components/Admin.js
+++ frontend/graceshopper/src/Components/Admin.js
@@ -1,37 +1,71 @@
 //Admin
-import React from "react";
+import React,{useState} from "react";
 import { Link } from 'react-router-dom';
 import styles from '../styles/Admin.module.css'
-// import { getAllItems } from "../api/itemRequests";
+import { getAllItems } from "../api/itemRequests";
 // import { getAllUsers } from '../api/users.js'
 
-const Admin = ({ token }) => {
+const Admin = () => {
 
-//   const [adminproducts, setAdminProducts] = useState([]);
+  const [adminproducts, setAdminProducts] = useState([]);
+
+  // useEffect(() => {
+  //     const getAllProductsAsync = async () => {
+  //         let allProducts = await getAllItems();
+  //         setAdminProducts(allProducts);
+  //     }
+  //     getAllProductsAsync();
+  // }, [])
 
   return (
-    <>
+    <div>
+      <header>
+        <h1 className={`${styles.title}`}>Admin</h1>
+      </header>
 
-      <h1 className={`${styles.title}`}>Admin</h1>
+      <div className={`${styles.subNav}`}>
+        <div className={`${styles.link}`}>
+          <Link to="/add-products">Add Products</Link>
+        </div>
 
-      {/* <button className={`btn btn-info ${styles.viewUsersButton}`} onClick={() => {console.log('View Users button clicked!')}}>View/Manage Users</button> */}
-      {/* <button className={`btn btn-info ${styles.viewUsersButton}`} onClick={() => {console.log('View Users button clicked!')}}>View/Manage Users</button> */}
-      {/* checking if user is admin, then show the add products button */}
-    
-    
-       {/* <button className={`btn btn-info ${styles.viewUsersButton}`} onClick={() => {console.log('View Users button clicked!')}}>View/Manage Users</button> */}
-       {/* checking if user is admin, then show the add products button */}      
-    
-    <div className={`${styles.subNav}`}>
-        <Link to="/add-products">Add Products</Link>
+        <button className={`${styles.getProducts}`}
+          onClick={async (event) => {
+
+            try {
+              event.preventDefault();
+              let getAdminProducts = await getAllItems();
+              setAdminProducts(getAdminProducts);
+            } catch (error) {
+              console.log(error, "error fetching products");
+            }}}
+        >Get Products</button>
+
+      <div className={`${styles.container}`}>
+
+        {adminproducts.map(({ brand, category, id, name, price, size, image, inventory }) => {
+          return (
+            <div key={id}>
+              <div className={`card ${styles.productCard}`}
+                style={{ width: "18rem", backgroundColor: "#B7E4C7" }}>
+
+                <img src={image} class="card-img-top" alt="..." />
+
+                <div class="card-body">
+                  <h5 class="card-title">Name</h5>
+                  <p class="card-text">{name}</p>
+                  <h5 class="card-title">Price</h5>
+                  <p class="card-text">{price}</p>
+                  <h5 class="card-title">Inventory</h5>
+                  <p class="card-text">{inventory}</p>
+                </div>
+
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
-
-      {/* <button className={`btn btn-secondary ${styles.viewProductsButton}`} onClick={() => {console.log('View Products button clicked!')}}>View/Manage Products</button> */}
-      {/* <button onClick={(e) => { 
-                getAllUsers();
-              }}>Get All Users</button>  */}
-     </>
-  )
-}
+  </div>
+)}
 
 export default Admin;
