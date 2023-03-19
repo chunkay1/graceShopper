90633b8e1759325cbdc809d7ad07b1ba9e3a2a62
Samson343 <sam.ban@gmail.com>
2023-03-18 18:56:16 -0600

    added cors back in

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const cors = require('cors')
+router.use(cors())
 
 const jwt = require('jsonwebtoken')
 const { JWT_SECRET } = process.env;
